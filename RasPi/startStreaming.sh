5fb9ab8717dbfd60a123f34ac6df814980a6af12
JuanNavarro <uan.navarro@datadivers.io>
2023-11-15 13:35:08 +0800

    start video streaming for 1 minute with raspi, python and aws IoT

--- RasPi/startStreaming.sh
+++ RasPi/startStreaming.sh
@@ -1,3 +1,11 @@
 export GST_PLUGIN_PATH='~/amazon-kinesis-video-streams-producer-sdk-cpp/build'
 export AWS_DEFAULT_REGION='ap-southeast-2'
+export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+cd ~/amazon-kinesis-video-streams-producer-sdk-cpp/build/
+
+
+
 ./kvs_gstreamer_sample RaspberryPiCamera
+
+wait  # Wait for child processes to exit
--- RasPi/turnCameraOn.py
+++ RasPi/turnCameraOn.py
@@ -1,7 +1,10 @@
 import subprocess
 import time
-import paho.mqtt.client as mqtt
+import threading
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient as mqttClient
 import logging
+import os
+import signal
 import json
 
 _logger = logging.getLogger(__name__)
@@ -9,21 +12,32 @@ _logger = logging.getLogger(__name__)
 
 def startStreaming():
     bash_script = './startStreaming.sh'
+    global trigger_flag
 
     # Start the bash script
-    process = subprocess.Popen(bash_script, shell=True)
-    time.sleep(300)
+    process = subprocess.Popen(['/bin/bash',bash_script], start_new_session=True)
+    time.sleep(60)
+
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+    
 
     process.terminate()
+    process.wait()
+    trigger_flag = True
+    threading.Timer(30.0, reset_trigger_flag).start()
     try:
         process.communicate(timeout=10)
     except subprocess.TimeoutExpired:
         process.kill()
 
 def turn_camera_on(value):
-    if value < 20:
+    global trigger_flag
+    if value < 10 and not trigger_flag:
         startStreaming()
-        _logger.info('camera turned on')
+        _logger.critical('camera turned on')
+def reset_trigger_flag():
+    global trigger_flag
+    trigger_flag = False
 
 def on_connect(client, userdata, flags, response_code):
     connackString = {0: 'Connection successful',
@@ -35,20 +49,40 @@ def on_connect(client, userdata, flags, response_code):
     _logger.critical(f'connected to broker: {connackString[response_code]}')
 
 def on_message(client, userdata, message):
-    _logger.info(f'received message: {message.payload.decode()}')
+    print(f'received message: {message.payload.decode()}')
+    _logger.critical(f'received message: {message.payload.decode()}')
     messageDict = json.loads(message.payload.decode())
     turn_camera_on(messageDict['value'])
-    client.publish('pong', 'received message')
 
-def mqtt_client(host:str, port:int):
-    mqttClient = mqtt.Client()
-    mqttClient.tls_set(ca_certs='./rootCA.pem', certfile='./certificate.pem.crt', keyfile='./private.pem.key')
-    mqttClient.on_connect = on_connect
-    mqttClient.on_message = on_message
-    mqttClient.connect(host, port=port)
+# def mqtt_client(host:str, port:int):
+#     mqttClient = mqtt.Client()
+#     mqttClient.tls_set(ca_certs='./readerKeys/rootCA.pem', certfile='./readerKeys/certificate.pem.crt', keyfile='./readerKeys/private.pem.key')
+#     mqttClient.on_connect = on_connect
+#     mqttClient.on_message = on_message
+#     mqttClient.connect(host, port=port)
+#     return mqttClient
     
 
 if __name__ == '__main__':
-    mqttClient = mqtt_client("a3nrtfu6i3fchr-ats.iot.ap-southeast-2.amazonaws.com", 8883)
-    mqttClient.subscribe('RasPi/data')
-    mqttClient.loop_forever()
+    trigger_flag = False
+    myMqttClient = mqttClient('myClient')
+    myMqttClient.configureEndpoint("a3nrtfu6i3fchr-ats.iot.ap-southeast-2.amazonaws.com", 8883)
+    myMqttClient.configureCredentials("./readerKeys/rootCA.pem", "./readerKeys/private.pem.key", "./readerKeys/certificate.pem.crt")
+    myMqttClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    myMqttClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+    myMqttClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    myMqttClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+    myMqttClient.connect()
+    myMqttClient.subscribe('RasPi/data', 1, on_message)
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print('Disconnecting...')
+        myMqttClient.disconnect()
+        print('Disconnected')
+
+    # mqttClient = mqtt_client("a3nrtfu6i3fchr-ats.iot.ap-southeast-2.amazonaws.com", 8883)
+    # mqttClient.subscribe('RasPi/data')
+    # mqttClient.loop_forever()
